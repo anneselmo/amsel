e14618f1df421c1da8bab318e465b85d114fc0c3
Anneselmo <anothergnome@tuta.io>
2018-05-09 16:04:52 +0000

    adding scripts

---
+++ scripts.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+import os
+
+#scripts that can be run inside of pages
+
+def gen_link(path, text):
+    return("<a href='"path"'>"text"</a>")
+
+def gen_index(path):
+    index=""
+    for item in os.listdir(path):
+        index=index+str(gen_link(str(path+item), item.split(".")[1]))
+    return(index) 
+
+        
