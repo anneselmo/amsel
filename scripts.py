b0208d6fc74f9af7537d48a306e9decdba19d291
Anneselmo <anothergnome@tuta.io>
2018-05-10 14:25:12 +0000

    time indexed blog listing

--- scripts.py
+++ scripts.py
@@ -12,3 +12,13 @@ def gen_index(path, root):
         index=index+str(gen_link(str(root+item), item.split(".")[0]))
     return(index)
 
+def gen_blog_index(path, root):
+    index="<p>"
+    flist=os.listdir(path)
+    flist.sort(reverse=True, key=lambda r: r.split(".")[0])
+    for item in flist:
+        index=index+"<h2>"+str(gen_link(str(root+item), item.split(".")[1]))+"</h2>"
+        index=index+item.split(".")[0]+"<br>"
+    return(index)
+
+
